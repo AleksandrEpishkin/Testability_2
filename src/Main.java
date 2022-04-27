d56e11f7c58d4240347fc6dc7fa04a8001a9c68a
AleksandrEpishkin <aepishkin@yandex.ru>
2022-04-27 16:24:50 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+
+        double bmi = service.calculate(80, 1.75);
+        System.out.println("Индекс массы тела: " + bmi);
+    }
+}
